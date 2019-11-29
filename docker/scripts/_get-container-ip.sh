04be391c58879d0c5e10a13ffda2a2ee36e7bffd
Serhii MOROZ <serhii.moroz@globallogic.com>
2019-11-29 13:21:04 +0200

    docker: additional tweaks

--- docker/scripts/_get-container-ip.sh
+++ docker/scripts/_get-container-ip.sh
@@ -5,5 +5,8 @@ cd ${DOCKER_ROOT_DIR}
 
 if is_linux; then
     # Available only in linux systems
+    echo '==========================================================================================================='
+    debug "The container IP is:"
     docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep -i "webserver"
+    echo '==========================================================================================================='
 fi
